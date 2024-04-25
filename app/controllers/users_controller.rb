881744022000f9069e7c107a4d21d734240ed217
Matthew Blott <m4ttb@pm.me>
2024-04-25 21:21:25 +0100

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,62 @@
+class UsersController < Clearance::UsersController
+  before_action :redirect_signed_in_users, only: [:create, :new]
+  skip_before_action :require_login, only: [:create, :new], raise: false
+
+  def new
+    @user = user_from_params
+    render(template: "users/new")
+  end
+
+  # def create
+  #   @user = user_from_params
+  #
+  #   if @user.save
+  #     sign_in(@user)
+  #     redirect_back_or(url_after_create)
+  #   else
+  #     render(template: "users/new", status: :unprocessable_entity)
+  #   end
+  # end
+
+  def create
+    @user = user_from_params
+
+    if @user.save
+      # sign_in(@user)
+
+      UserMailer.with(user: @user).welcome_email.deliver_later
+
+      # format.html { redirect_to(@user, notice: 'User was successfully created.')
+
+      redirect_back_or(url_after_create)
+    else
+      render(template: "users/new", status: :unprocessable_entity)
+    end
+  end
+
+  private
+
+  def redirect_signed_in_users
+    if signed_in?
+      redirect_to(Clearance.configuration.redirect_url)
+    end
+  end
+
+  def url_after_create
+    Clearance.configuration.redirect_url
+  end
+
+  def user_from_params
+    email = user_params.delete(:email)
+    password = user_params.delete(:password)
+
+    Clearance.configuration.user_model.new(user_params).tap do |user|
+      user.email = email
+      user.password = password
+    end
+  end
+
+  def user_params
+    params[Clearance.configuration.user_parameter] || {}
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+  get("home/index")
+  resources(:passwords, controller: "clearance/passwords", only: [:create, :new])
+  resource(:session, controller: "clearance/sessions", only: [:create])
+
+  resources(:users, controller: "users", only: [:create]) do
+    resource(
+      :password,
+      controller: "clearance/passwords",
+      only: [:edit, :update]
+    )
+  end
+
+  get("/sign_in" => "clearance/sessions#new", :as => "sign_in")
+  delete("/sign_out" => "clearance/sessions#destroy", :as => "sign_out")
+  get("/sign_up" => "users#new", :as => "sign_up")
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get("up" => "rails/health#show", :as => :rails_health_check)
+  # Defines the root path route ("/")
+  root("home#index")
+end
